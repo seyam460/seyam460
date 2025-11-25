cde3fca3c351ac01b8854630aff5cf2e30cf78b9
seyam460 <sseyam450@gmail.com>
2025-11-25 20:22:06 +0600

    Add files via upload

---
+++ greater average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+     int T;
+     scanf("%d",&T);
+
+       while(T--){
+        int n,x;
+        scanf("%d %d",&n,&x);
+        int subs=(n+5)/6;
+        int cost=subs * x;
+        printf("%d",&cost);
+       }
+       return 0;
+}
