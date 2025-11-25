c26e4355c263cca06747c6d61515647cfca0aa0b
seyam460 <sseyam450@gmail.com>
2025-11-25 20:20:17 +0600

    Add files via upload

---
+++ binary explamation.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int binarysearchfirst(int arr[],int n,int k){
+    int low=0,high=n-1;
+    int result=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(arr[mid] == k){
+            result=mid;
+            high=mid-1;
+        }
+        else if(arr[mid]<k){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return result;
+}
+int main(){
+    int arr[]={1,1,1,1,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=1;
+
+    int index=binarysearchfirst(arr,n,k);
+    printf("%d",index);
+
+    return 0;
+}
