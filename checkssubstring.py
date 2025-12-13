dd8e531f970a6051ecef611d0580e4c3f93b641e
seyam460 <sseyam450@gmail.com>
2025-12-13 21:15:11 +0600

    Add files via upload

---
+++ checkssubstring.py
@@ -0,0 +1,8 @@
+String = input("enter the main string: ")
+substring = input ("enter the substring: ")
+
+if substring in String:
+    print ("substring is present in the string")
+
+else :
+    print ("substring is not present in the string")
