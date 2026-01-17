32e847a6890faf9ae0cd3ff2e2cc2717aeab767d
seyam460 <sseyam450@gmail.com>
2026-01-18 00:19:40 +0600

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+n = int(input("enter a number:"))
+if n%2 == 1:
+    print("weired")
+else:
+    if 2<= n <= 5:
+        print("not weired")
+    elif 6<= n <= 20:
+        print("weired")
+    else:
+        print("not weired")
