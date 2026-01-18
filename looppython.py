f67dd0326d4eb38e28ed0b8978026e7dc04e8bed
seyam460 <sseyam450@gmail.com>
2026-01-19 01:05:15 +0600

    Add files via upload

---
+++ looppython.py
@@ -0,0 +1,25 @@
+li = range(1, 11)
+print(type(li))
+for x in li:
+    print(x)
+
+li = list(range(1, 11))
+print(li)
+ 
+even_numbers = list(range(2, 101, 2))
+print(even_numbers)
+
+numbers = list(range(5,100,5))
+print(numbers)
+
+numbers = list(range(1,100,-5))
+print(numbers)
+
+numbers = list(range(10,0,-2))
+print(numbers)
+
+numbers = list(range(100,6,-4))
+print(numbers)
+
+
+               
